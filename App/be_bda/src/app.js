f6e767b6e10cff7993d696ade820b0f5c96c92af
gchougale <gchougale32@gmail.com>
2025-09-25 12:29:44 +0530

    saved

---
+++ App/be_bda/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to BDA Backend API' });
+});
+
+module.exports = app;
